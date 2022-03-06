58016678de0ec22ddeb06049ac18017944ccbd0c
ckvaizz <vaisakh.k591@gmail.com>
2022-03-06 12:27:26 +0530

    added admin stuffs

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const  User =require('../models/user')
-
+const Medi =require('../models/medi')
 exports.getShopkeepers=async(req,res)=>{
     try {
         const {status}= req.body
@@ -11,5 +11,26 @@ exports.getShopkeepers=async(req,res)=>{
 }
 
 exports.deleteUser=async(req,res)=>{
-    try
+    try {
+        const {_id}=req.body
+        if((await User.deleteOne({_id})).deletedCount===1){
+            await Medi.deleteMany({shopId:_id})
+            res.json({status:true})
+        }else{
+            res.json({status:false,message:'Failed to delete'})
+        }
+    } catch (error) {
+        res.json({status:false,message:'something went wrong'})
+    }
+}
+
+exports.approveSk=async(req,res)=>{
+    try {
+        const {_id}= req.body
+        if((await User.updateOne({_id},{status:true})).modifiedCount==1)
+            res.json({status:true})
+        else res.json({status:false,message:'Failed to approve'})
+    } catch (error) {
+        res.json({status:false,message:'something wrong'})
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,14 @@
 var express = require('express');
 
-const { getShopkeepers } = require('../controllers/users');
+const { getShopkeepers, deleteUser, approveSk } = require('../controllers/users');
 var router = express.Router();
 const { auth, isAdmin, checkValidator }=require('../middleware/auth');
 const { statusValidator, idValidator } = require('../middleware/validator');
 
 router.post('/get',auth,isAdmin,statusValidator,checkValidator,getShopkeepers)
 
-router.post('delete',auth,isAdmin,idValidator,checkValidator,deleteUser)
+router.post('/delete',auth,isAdmin,idValidator,checkValidator,deleteUser)
+
+router.post('/approve',auth,isAdmin,idValidator,approveSk)
+
 module.exports = router;
